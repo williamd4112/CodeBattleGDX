5a940172bfa1a13fed2e5dd0c02fd597b948e04c
williamd4112 <williamd4112@hotmail.com>
2014-12-17 16:36:26 +0800

    Conflict Solving

--- core/src/com/codebattle/model/GameActor.java
+++ core/src/com/codebattle/model/GameActor.java
@@ -9,164 +9,159 @@ import com.codebattle.utility.GameUnits.Interval;
 import com.codebattle.utility.GameUnits.Speed;
 import com.codebattle.utility.TextureFactory;
 
-/**
- * Game actor.
- *
+/*
+ * GameActor
  * @name : actor's name , used to load the corresponding resource
- */
-public class GameActor extends Actor {
-    // Actor's direction
-    private Direction direction;
-    // Texture's regions
-    private final TextureRegion[][] frames;
-    // Animation update interval
-    private final Interval interval;
-    // Animation frame index
-    private int frameIndex;
-    // Animation frame count
-    private int frameCount;
-    private final Speed speed;
+ * @direction : actor's direction
+ * @interval : animation update frequency
+ * @frameIndex : animation frame index
+ * @frameCount : animation frame count
+ * @frames : texture's regions
+ * */
 
-    /**
-     * Create a game actor.
-     *
-     * @param name  Actor name, used to load the corresponding resources
-     * @param sx    Position X
-     * @param sy    Position Y
-     */
-    public GameActor(final String name, final float sx, final float sy) {
-        super();
-        this.setName(name);
-        this.frames = TextureFactory.getInstance()
-                .loadTextureRegionFromFile(name, GameUnits.CHR_VSLICES, GameUnits.CHR_HSLICES,
-                        GameUnits.CHR_WIDTH, GameUnits.CHR_HEIGHT);
-        this.direction = Direction.DOWN;
-        this.frameIndex = 0;
-        this.frameCount = 0;
-        this.interval = Interval.HIGH;
-        this.speed = Speed.VERYFAST;
-        this.setX(sx);
-        this.setY(sy);
-    }
+public class GameActor extends Actor
+{	
+	private Direction direction;
+	private TextureRegion[][] frames;
+	private Interval interval;
+	private int frameIndex;
+	private int frameCount;
+	private Speed speed;
+		
+	public GameActor(String name , float sx , float sy)
+	{
+		super();
+		this.setName(name);
+		this.frames = TextureFactory.getInstance().loadTextureRegionFromFile(name, 
+				GameUnits.CHR_VSLICES, GameUnits.CHR_HSLICES, GameUnits.CHR_WIDTH, GameUnits.CHR_HEIGHT);
+		this.direction = Direction.DOWN;
+		this.frameIndex = 0;
+		this.frameCount = 0;
+		this.interval = Interval.HIGH; 
+		this.speed = Speed.VERYFAST;
+		this.setX(sx);
+		this.setY(sy);
+	}
+	
+	/*User interface : turn*/
+	public void turnDown()
+	{
+		this.setDirection(Direction.DOWN);
+	}
+	
+	public void turnLeft()
+	{
+		this.setDirection(Direction.LEFT);
+	}
+	
+	public void turnRight()
+	{
+		this.setDirection(Direction.RIGHT);
+	}
+	
+	public void turnUp()
+	{
+		this.setDirection(Direction.UP);
+	}
+	
+	/*User interface : move*/
+	public void moveDown(int pace)
+	{
+		this.move(pace, Direction.DOWN);
+	}
+	
+	public void moveLeft(int pace)
+	{
+		this.move(pace, Direction.LEFT);
+	}
+	
+	public void moveRight(int pace)
+	{
+		this.move(pace, Direction.RIGHT);
+	}
+	
+	public void moveUp(int pace)
+	{
+		this.move(pace, Direction.UP);
+	}
+	
+	/*Stop animation , reseting all animation variables*/
+	private void stop()
+	{
+		this.frameCount = 0;
+		this.frameIndex = 0;
+	}
+	
+	/*Set a new direction for this actor*/
+	private void setDirection(Direction dir)
+	{
+		this.direction = dir;
+	}
 
-    /*
-     * User interface: Turn
-     */
+	/*Move to specific direction in a paces*/
+	private void move(int pace , Direction dir)
+	{
+		try {
+			this.setDirection(dir);
+			for(float diff = pace * GameUnits.CELL_SIZE ; diff > 0 ; diff -= speed.Val()) {
+				switch(dir) {
+				case DOWN:
+					this.moveBy(0 , -speed.Val());
+					break;
+				case LEFT:
+					this.moveBy(-speed.Val() , 0);
+					break;
+				case RIGHT:
+					this.moveBy(speed.Val() , 0);
+					break;
+				case UP:
+					this.moveBy(0 , speed.Val());
+					break;
+				default:
+					break;
+				}
+				Thread.sleep(GameUnits.SLEEP_TIME);
+			}
+			stop();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void moveBy(float x, float y) {
+		// TODO Auto-generated method stub
+		super.moveBy(x, y);
+		frameCount = (frameCount < interval.Val()) ? frameCount + 1 : 0;
+		
+		if(frameCount >= interval.Val())
+			frameIndex = (frameIndex < GameUnits.ANI_CYCLE) ? frameIndex + 1 : 0;
+	}
 
-    public void turnDown() {
-        this.setDirection(Direction.DOWN);
-    }
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		super.draw(batch, parentAlpha);
+		batch.draw(frames[this.direction.Val()][frameIndex], this.getX(), this.getY());
+	}
 
-    public void turnLeft() {
-        this.setDirection(Direction.LEFT);
-    }
-
-    public void turnRight() {
-        this.setDirection(Direction.RIGHT);
-    }
-
-    public void turnUp() {
-        this.setDirection(Direction.UP);
-    }
-
-    /*
-     * User interface: Move
-     */
-
-    public void moveDown(final int pace) {
-        this.move(pace, Direction.DOWN);
-    }
-
-    public void moveLeft(final int pace) {
-        this.move(pace, Direction.LEFT);
-    }
-
-    public void moveRight(final int pace) {
-        this.move(pace, Direction.RIGHT);
-    }
-
-    public void moveUp(final int pace) {
-        this.move(pace, Direction.UP);
-    }
-
-    /**
-     * Stop the animation and reset all variables.
-     */
-    private void stop() {
-        this.frameCount = 0;
-        this.frameIndex = 0;
-    }
-
-    /**
-     * Set a new direction for this actor.
-     *
-     * @param dir   Direction
-     */
-    private void setDirection(final Direction dir) {
-        this.direction = dir;
-    }
-
-    /**
-     * Move to specific direction by many paces.
-     *
-     * @param pace  Pace
-     * @param dir   Direction
-     */
-    private void move(final int pace, final Direction dir) {
-        try {
-            this.setDirection(dir);
-
-            for (float diff = pace * GameUnits.CELL_SIZE; diff > 0; diff -= this.speed.getValue()) {
-                switch (dir) {
-                case DOWN: {
-                    this.moveBy(0, -this.speed.getValue());
-                    break;
-                }
-                case LEFT: {
-                    this.moveBy(-this.speed.getValue(), 0);
-                    break;
-                }
-                case RIGHT: {
-                    this.moveBy(this.speed.getValue(), 0);
-                    break;
-                }
-                case UP: {
-                    this.moveBy(0, this.speed.getValue());
-                    break;
-                }
-                default:
-                    break;
-                }
-
-                Thread.sleep(GameUnits.SLEEP_TIME);
-            }
-
-            this.stop();
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void moveBy(final float x, final float y) {
-        // TODO Auto-generated method stub
-        super.moveBy(x, y);
-        this.frameCount = this.frameCount < this.interval.getValue() ? this.frameCount + 1 : 0;
-
-        if (this.frameCount >= this.interval.getValue()) {
-            this.frameIndex = this.frameIndex < GameUnits.ANI_CYCLE ? this.frameIndex + 1 : 0;
-        }
-    }
-
-    @Override
-    public void draw(final Batch batch, final float parentAlpha) {
-        super.draw(batch, parentAlpha);
-        batch.draw(this.frames[this.direction.getValue()][this.frameIndex], this.getX(),
-                this.getY());
-    }
-
-    @Override
-    public void act(final float delta) {
-        super.act(delta);
-    }
+	@Override
+	public void act(float delta) {
+		super.act(delta);
+	}
+	
+	/**
+	 * Get a row (specific direction) frames
+	 * @param dir
+	 * @return
+	 */
+	public TextureRegion[] getDirectionFrames(Direction dir)
+	{
+		return this.frames[dir.Val()];
+	}
+	
+	public TextureRegion[] getCurrentDirectionFrames()
+	{
+		return this.frames[this.direction.Val()];
+	}
+	
 }
--- core/src/com/codebattle/utility/GameUnits.java
+++ core/src/com/codebattle/utility/GameUnits.java
@@ -1,53 +1,88 @@
 package com.codebattle.utility;
 
 public class GameUnits {
-    public static final int CELL_SIZE = 32;
-    public static final int CHR_VSLICES = 4;
-    public static final int CHR_HSLICES = 4;
-    public static final int CHR_WIDTH = 32;
-    public static final int CHR_HEIGHT = 48;
-    public static final int ANI_CYCLE = 3;
-    public static final int SLEEP_TIME = 10;
-
-    public enum Speed {
-        NORMAL(0.2f), FAST(0.4f), VERYFAST(0.8f);
-
-        private final float value;
-
-        Speed(final float value) {
-            this.value = value;
-        }
-
-        public float getValue() {
-            return this.value;
-        }
-    }
-
-    public enum Interval {
-        NORMAL(30), HIGH(20), VERYHIGH(10);
-
-        private final int value;
-
-        Interval(final int value) {
-            this.value = value;
-        }
-
-        public float getValue() {
-            return this.value;
-        }
-    }
-
-    public enum Direction {
-        DOWN(0), LEFT(1), RIGHT(2), UP(3);
-
-        private final int value;
-
-        private Direction(final int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return this.value;
-        }
-    }
+	
+	/**
+	 * Constants of the global game
+	 * (be able to modified in config file)
+	 */
+	public final static int FRAMERATE = 25;
+	public final static int CELL_SIZE = 32;
+	public final static int CHR_VSLICES = 4;
+	public final static int CHR_HSLICES = 4;
+	public final static int CHR_WIDTH = 32;
+	public final static int CHR_HEIGHT = 48;
+	public final static int ANI_CYCLE = 3; 
+	public final static int SLEEP_TIME = 10;
+	
+	/**
+	 * Enumeration of speed
+	 * NORMAL
+	 * FAST
+	 * VERYFAST
+	 * @author williamd
+	 */
+	public enum Speed{
+		NORMAL(0.2f),
+		FAST(0.4f),
+		VERYFAST(0.8f);
+		
+		private float val;
+		Speed(float val)
+		{
+			this.val = val;
+		}
+		
+		public float Val()
+		{
+			return this.val;
+		}
+	}
+	
+	/**
+	 * Enumeration of Interval(Animation Frame switch interval , not translation interval)
+	 * @author williamd
+	 *
+	 */
+	public enum Interval{
+		NORMAL(30),
+		HIGH(20),
+		VERYHIGH(10);
+		
+		private int val;
+		Interval(int val){
+			this.val = val;
+		}
+		
+		public float Val()
+		{
+			return this.val;
+		}
+	}
+	
+	/**
+	 * Enumeration of Direction
+	 * @author williamd
+	 *
+	 */
+	public enum Direction
+	{
+		DOWN(0),
+		LEFT(1),
+		RIGHT(2),
+		UP(3);
+		
+		final private int val;
+		private Direction(int value)
+		{
+			this.val = value;
+		}
+		
+		public int Val()
+		{
+			return this.val;
+		}
+		
+	}
+	
 }
